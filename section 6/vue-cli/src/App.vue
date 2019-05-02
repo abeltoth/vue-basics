efce401a3b75d17ae9c14b9db5eff2b2b7eb5aee
Abel Toth <abel.toth@nuage.hu>
2019-05-02 20:30:07 +0200

    Components project

--- section 6/vue-cli/src/App.vue
+++ section 6/vue-cli/src/App.vue
@@ -3,6 +3,11 @@
 </template>
 
 <script>
+/* sudo npm install -g vue cli */
+/* github.com/vuejs/vue-cli */
+/* vue init webpack-simple vue-cli */
+/* npm install */
+/* npm run dev */
 export default {
 }
 </script>
