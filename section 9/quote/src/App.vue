f92c5c42cb8b715fd12f761309c5d5a5de7c3619
Abel Toth <abel.toth@nuage.hu>
2019-05-07 13:06:24 +0200

    Slots

---
+++ section 9/quote/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <app-quote>
+          <h2 slot="title">{{ quoteTitle }}</h2>
+          <p slot>Wonderful</p>
+        </app-quote>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from './components/Quote';
+
+export default {
+  data() {
+    return {
+      quoteTitle: 'The Quote'
+    }
+  },
+  components: {
+    appQuote: Quote
+  }
+}
+</script>
+
+<style>
+
+</style>
