2ea4ffc68208a212e687690ad72c643def4f3177
Abel Toth <abel.toth@nuage.hu>
2019-05-08 11:29:39 +0200

    Quote app finished

--- section 10/quote-app/src/App.vue
+++ section 10/quote-app/src/App.vue
@@ -1,11 +1,20 @@
 <template>
     <div class="container">
-      <app-quote-grid :quotes="quotes"></app-quote-grid>
+      <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+      <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+      <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+      <div class="row">
+        <div class="col-sm-12 text-center">
+          <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+        </div>
+      </div>
     </div>
 </template>
 
 <script>
-  import QuoteGrid from './components/QuoteGrid.vue'
+  import QuoteGrid from './components/QuoteGrid.vue';
+  import NewQuote from './components/NewQuote.vue';
+  import Header from './components/Header.vue';
   export default {
     data() {
       return {
@@ -15,8 +24,21 @@
         maxQuotes: 10
       }
     },
+    methods: {
+      newQuote(quote) {
+        if (this.quotes.length >= this.maxQuotes) {
+          return alert('Please delete Quotes first!');
+        }
+        this.quotes.push(quote);
+      },
+      deleteQuote(index) {
+        this.quotes.splice(index, 1);
+      }
+    },
     components: {
-      appQuoteGrid: QuoteGrid
+      appQuoteGrid: QuoteGrid,
+      appNewQuote: NewQuote,
+      appHeader: Header
     }
   }
 </script>
